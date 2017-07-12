0cb82df56df9da4b4f52e62d40c1f5be310ec5f4
Naomi Quispe <nquispe@xogrp.com>
2017-07-12 18:11:10 -0400

    Made a new twitter client

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Node is running!')
+var twitter = require('Twitter')
+
+var client = new Twitter({
+  consumer_key = process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret = process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key = process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret = process.env.TWITTER_ACCESS_TOKEN_SECRET
+})
