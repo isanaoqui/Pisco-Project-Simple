c8d576f8f210ef2c33db1c5ea1be9114570ef7e1
Naomi Quispe <nquispe@xogrp.com>
2017-07-31 15:05:20 -0400

    Added express server & main.js file

--- dataPeru.js
+++ dataPeru.js
@@ -4,6 +4,6 @@ var searchPeru = twitter.searchPeru
 
 client.get('search/tweets', {q: searchPeru}, function(error, tweets, response) {
   if(error) throw error
-  
+
   console.log(tweets.statuses.length)
 })
