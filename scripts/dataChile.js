c7a5e3513c7cd67b72c76122e784dd4f7c42ae34
Naomi Quispe <nquispe@xogrp.com>
2017-07-31 15:55:19 -0400

    Exported numbers found of current pisco tweets

--- scripts/dataChile.js
+++ scripts/dataChile.js
@@ -1,8 +1,12 @@
 const twitter = require('./twitter')
 const client = twitter.client
 var searchChile = twitter.searchChile
+// var numChile = 0
 
 client.get('search/tweets', {q: searchChile}, function(error, tweets, response) {
   if(error) throw error
-  console.log(tweets.statuses.length)
+  exports.numChile = tweets.statuses.length
+  console.log("chile: " + tweets.statuses.length)
 })
+
+// exports.numChile = numChile
--- scripts/dataPeru.js
+++ scripts/dataPeru.js
@@ -1,9 +1,12 @@
 const twitter = require('./twitter')
 const client = twitter.client
 var searchPeru = twitter.searchPeru
+// var numPeru = 0
 
 client.get('search/tweets', {q: searchPeru}, function(error, tweets, response) {
   if(error) throw error
-
-  console.log(tweets.statuses.length)
+  exports.numPeru = tweets.statuses.length
+  console.log("peru: " + tweets.statuses.length)
 })
+
+// exports.numPeru = numPeru
